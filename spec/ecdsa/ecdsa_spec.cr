a5fbe76503a0e6d639f6b009a4ab6a7e0fe00575
Alex Kampa <aleksander.kampa@gmail.com>
2021-08-24 00:01:14 +0200

    Ethereum signature verification

--- spec/ecdsa/ecdsa_spec.cr
+++ spec/ecdsa/ecdsa_spec.cr
@@ -15,4 +15,32 @@ describe ECDSA do
 
   end
 
+  it "verify Ethereum signed message #1" do
+    data = "Hello, world!"
+    signature = "0x643372b9c0636aa8757703ebfdd59d149ac1f9c2cdb66ee569812fe89ee963cb56e4d5d28843edeb0e529431bf724419987002302955979e92b64b6b6683817e1c"
+    eth_account = "0xeB61E703DB916935A9B435eeaFa132cA59dc0BbB"
+    ECDSA.verify_ethereum_signature(data, signature, eth_account).should eq true
+  end
+
+  it "verify Ethereum signed message #2" do
+    data = "Hello, world!"
+    signature = "0x1038030f8b327e0ceb7a92b9047fa3bdf43cde80f90f3593478de1796d898b4c2b409adf536062205fb68aa68f842c5c4e163e7228d47999e12af47f128c2a931c"
+    eth_account = "0x001d3f1ef827552ae1114027bd3ecf1f086ba0f9"
+    ECDSA.verify_ethereum_signature(data, signature, eth_account).should eq true
+  end
+
+  it "verify Ethereum signed message #3" do
+    data = "Hello, world!"
+    signature = "0x7c34d821ff8a6ca0b5c7cc080551994151ca3e4a0ac655deb659bc9aabaab0ca0027d20f7c9affb3e915d1be3b9b6ef28544f6ff771bd88d4627323a8adfd3e001"
+    eth_account = "0x2172B6F29d1F594A66424D4261efe2604934f25e"
+    ECDSA.verify_ethereum_signature(data, signature, eth_account).should eq true
+  end
+
+  it "verify Ethereum signed message #4" do
+    data = "Hello, world!!"
+    signature = "0xc59477104ff48738c4f2b81b4028a950e3c22ded6698d60f884b34a3c216ad0a381f66099702dd99dd6d601cd1c32e4d35c158cbad8c150aa95ecc07855ac8bc00"
+    eth_account = "0x2172B6F29d1F594A66424D4261efe2604934f25e"
+    ECDSA.verify_ethereum_signature(data, signature, eth_account).should eq true
+  end
+
 end
--- src/ecdsa.cr
+++ src/ecdsa.cr
@@ -29,4 +29,39 @@ module ECDSA
     return "0x#{hash[-40..-1]}"
   end
 
+  # Ethereum Signature Verification
+  #
+  # This function verifies if a data string has been signed by a given Ethereum address
+  # Cf https://web3js.readthedocs.io/en/v1.2.11/web3-eth-personal.html#sign
+  #
+  # Note that Ethereum uses determnistic signatures as per RFC 6979
+  # which means that the signature will always be the same
+  #
+  # We expect the signature to have length 130, consisting of:
+  #
+  # 0..63 : hex value of r
+  # 64..127 : hex value of s
+  # 128..129 : "1b"/"1c" (Metamask) or "00"/"01" (Ledger) for even/odd y value
+  #
+  # Signatures of length 132, starting with 0x, are also acceptable
+  #
+  # To obtain a signature, for example in the Firefox browser console (F12)
+  # when web3 has been initialised and an account is connected, the command is:
+  # > account = "0x.....";
+  # > web3.eth.personal.sign("Hello, world!", account, "").then(console.log);
+
+  def self.verify_ethereum_signature(data : String, signature : String, eth_account : String) : Bool
+    g = ECDSA.get_group :secp256k1
+    sig = signature.gsub(/^0x/, "")
+    res = Digest::Keccak.hexdigest "\u0019Ethereum Signed Message:\n#{data.size}#{data}"
+    h = BigInt.new(res, base: 16)
+    r = BigInt.new(signature[2..65], base: 16)
+    s = BigInt.new(signature[66..129], base: 16)
+    even = signature[130..131] =~ /1b|00/ ? true : false
+    public_key = g.recover_public_key(h, r, s, even)
+    eth_recovered = ECDSA.eth_address(public_key)
+    return true if eth_recovered == eth_account.downcase
+    return false
+  end
+
 end
