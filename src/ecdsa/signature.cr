6258d97eb125e35bfb19600f8017f1b427eaa1dd
Alex Kampa <aleksander.kampa@gmail.com>
2021-10-19 16:16:55 +0200

    Update signature.cr
    bug fix - padding with 0 need in some cases

--- src/ecdsa/signature.cr
+++ src/ecdsa/signature.cr
@@ -15,7 +15,7 @@ module ECDSA
       #
       # this is an Ethereum-style signature 
       #
-      return "0x#{r.to_s(16)}#{s.to_s(16)}#{(27+v).to_s(16)}"
+      return "0x#{r.to_s(16).rjust(64, '0')}#{s.to_s(16).rjust(64, '0')}#{(27+v).to_s(16)}"
     end
   end
 end
